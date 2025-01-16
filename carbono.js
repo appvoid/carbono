92d632e0a60e2401949bd75ff0b39ede5ce8ba4c
appvoid <78444142+appvoid@users.noreply.github.com>
2025-01-15 21:50:36 -0400

    Update carbono.js

--- carbono.js
+++ carbono.js
@@ -1,8 +1,8 @@
 /* 
  *  this is a complete example of a standard feed-forward neural network and ideally, in the future, this self-contained model is planned to be 
  *  sufficient to handle general use-case scenarios as well as becoming a supportive, general-purpose engine for training as well as 
- *  inference on most popular frameworks' models out there - having said that, be warned, as most of my indie open source projects, 
- *  this is not by any means stable yet, even though i'm expecting the tool to be improved over time, you must use it at your own risk.
+ *  inference on pytorch; this is not by any means stable yet, even though i'm expecting the tool to be improved over time, you must use it 
+ *  at your own risk.
  */
 class carbono {
   // 🏗️ Constructor: Initializes the neural network with default settings
